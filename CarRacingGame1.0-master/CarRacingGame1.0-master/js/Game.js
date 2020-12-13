64848605a8e566f290f93f4c1f5044efc74c1bec
bababoy80 <73437642+bababoy80@users.noreply.github.com>
2020-12-13 12:48:15 -0500

    Add files via upload

---
+++ CarRacingGame1.0-master/CarRacingGame1.0-master/js/Game.js
@@ -0,0 +1,62 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+  } 
+  car1=createSprite(20,90,50,50) 
+  car2=createSprite(20,140,50,50) 
+  car3=createSprite(20,190,50,50) 
+  car4=createSprite(20,240,50,50)
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      for(var plr in allPlayers){
+        if (plr === "player" + player.index)
+          fill("red")
+        else
+          fill("black");
+
+        display_position+=20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    } 
+    drawSprites() 
+    
+  }
+}
